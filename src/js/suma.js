2069a33cff707fdbf91bc8bdd5dcfd089bdaf1ec
oscarjavier12 <soy767mex@gmail.com>
2025-02-07 19:39:59 -0600

    Realizar suma

--- src/js/suma.js
+++ src/js/suma.js
@@ -1,3 +1,9 @@
 let num1 = prompt("Introduce el primer número");
 let num2 = prompt("Introduce el segundo número");
-alert(`La suma de ${num1} y ${num2} es ${parseInt(num1) + parseInt(num2)}`);
+//alert(`La suma de ${num1} y ${num2} es ${num1 + num2}`); //covertir los numeros a enteros
+//covertir los numeros a enteros
+//alert(`La suma de ${num1} y ${num2} es ${parseInt(num1) + parseInt(num2)}`);
+// Con el + delante de num1 y num2, los convierte a número
+//alert(`La suma de ${num1} y ${num2} es ${+num1 + +num2}`);
+// Con Number() convierte a número
+alert(`La suma de ${num1} y ${num2} es ${Number(num1) + Number(num2)}`);
